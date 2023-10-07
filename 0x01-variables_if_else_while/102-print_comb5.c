2cc503b2bc3a974290f5cd630ae2de48ed56de57
YesuGi <yesunehgizaw@gmail.com>
2023-10-06 18:32:16 -0700

    if else project alx

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,16 +8,14 @@ int main(void)
     {
         for (j = i; j <= 99; j++)
         {
-            putchar((i / 10) / 10 + '0');
-            putchar((i / 10) % 10 + '0');
-            putchar((i % 10) / 10 + '0');
-            putchar((i % 10) % 10 + '0');
+            putchar(i / 10 + '0');
+            putchar(i % 10 + '0');
             putchar(' ');
 
-            putchar((j / 10) / 10 + '0');
-            putchar((j / 10) % 10 + '0');
-            putchar((j % 10) / 10 + '0');
-            putchar((j % 10) % 10 + '0');
+            putchar(j / 10 / 10 + '0');
+            putchar(j / 10 % 10 + '0');
+            putchar(j % 10 / 10 + '0');
+            putchar(j % 10 % 10 + '0');
 
             if (i != 99 || j != 99)
             {
