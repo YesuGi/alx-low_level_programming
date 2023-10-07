da47a79e5201b13fad915dcf16b6f120078aad6b
YesuGi <yesunehgizaw@gmail.com>
2023-10-06 17:26:53 -0700

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 
+/**
+ * main - Prints the digits from 0 to 9.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-    putchar('0');
-    putchar('1');
-    putchar('2');
-    putchar('3');
-    putchar('4');
-    putchar('5');
-    putchar('6');
-    putchar('7');
-    putchar('8');
-    putchar('9');
-    putchar('\n');
+	putchar('0');
+	putchar('1');
+	putchar('2');
+	putchar('3');
+	putchar('4');
+	putchar('5');
+	putchar('6');
+	putchar('7');
+	putchar('8');
+	putchar('9');
+	putchar('\n');
 
-    return 0;
+	return (0);
 }
-
