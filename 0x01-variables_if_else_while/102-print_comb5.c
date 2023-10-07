e55474eb4211be6584723523c8a2f4d177495e25
YesuGi <yesunehgizaw@gmail.com>
2023-10-06 18:28:20 -0700

    if else project alx

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,15 +2,19 @@
 
 int main(void)
 {
-    for (int i = 0; i <= 99; i++)
-    {
-        int firstDigit1 = i / 10;
-        int secondDigit1 = i % 10;
+    int i, j;
+    int firstDigit1, secondDigit1;
+    int firstDigit2, secondDigit2;
 
-        for (int j = i; j <= 99; j++)
+    for (i = 0; i <= 99; i++)
+    {
+        firstDigit1 = i / 10;
+        secondDigit1 = i % 10;
+
+        for (j = i; j <= 99; j++)
         {
-            int firstDigit2 = j / 10;
-            int secondDigit2 = j % 10;
+            firstDigit2 = j / 10;
+            secondDigit2 = j % 10;
 
             putchar(firstDigit1 / 10 + '0');
             putchar(firstDigit1 % 10 + '0');
@@ -22,7 +26,6 @@ int main(void)
             putchar(firstDigit2 % 10 + '0');
             putchar(secondDigit2 / 10 + '0');
             putchar(secondDigit2 % 10 + '0');
-
             if (i != 99 || j != 99)
             {
                 putchar(',');
