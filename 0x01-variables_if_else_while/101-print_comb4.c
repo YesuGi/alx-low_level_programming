02cc7500ed703656a6cf5ed012edad745dc529dc
YesuGi <yesunehgizaw@gmail.com>
2023-10-06 18:10:21 -0700

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,26 +1,20 @@
 #include <stdio.h>
 
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success).
+ */
 int main(void)
 {
-    for (int i = 0; i < 8; i++)
+    char letter;
+
+    for (letter = 'a'; letter <= 'z'; letter++)
     {
-        for (int j = i + 1; j < 9; j++)
-        {
-            for (int k = j + 1; k <= 9; k++)
-            {
-                putchar(i + '0');
-                putchar(j + '0');
-                putchar(k + '0');
-                if (i != 7 || j != 8 || k != 9)
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
+        putchar(letter);
     }
 
     putchar('\n');
 
-    return 0;
+    return (0);
 }
