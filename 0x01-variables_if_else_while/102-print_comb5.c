346561f22371372b9bf55045b796eb9afe067132
YesuGi <yesunehgizaw@gmail.com>
2023-10-06 18:59:59 -0700

    if else project alx

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,32 +1,29 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible combinations of two two-digit numbers
- * Return: Always 0 (Success)
+ * main - Prints all possible combinations of two two-digit numbers.
+ * Return: Always 0 (Success).
  */
 int main(void)
 {
     int i, j;
 
-    for (i = 0; i <= 100; i++)
+    for (i = 0; i <= 99; i++)
     {
-        for (j = i; j <= 100; j++)
+        for (j = i + 1; j <= 99; j++)
         {
-            if (i < j)
+            putchar(i / 10 + '0');
+            putchar(i % 10 + '0');
+            putchar(' ');
+            putchar(j / 10 + '0');
+            putchar(j % 10 + '0');
+            if (i != 98 || j != 99)
             {
-                putchar(i / 10 + 48);
-                putchar(i % 10 + 48);
+                putchar(',');
                 putchar(' ');
-                putchar(j / 10 + 48);
-                putchar(j % 10 + 48);
-                if (i != 98 || j != 99)
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
             }
         }
     }
     putchar('\n');
-    return (0);
+    return 0;
 }
