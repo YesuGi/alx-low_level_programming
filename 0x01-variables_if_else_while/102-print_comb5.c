79fdcd0856e67dffd5db0e8728ed0138c3a01684
YesuGi <yesunehgizaw@gmail.com>
2023-10-06 18:30:45 -0700

    if else project alx

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,38 +3,38 @@
 int main(void)
 {
     int i, j;
-    int firstDigit1, secondDigit1;
-    int firstDigit2, secondDigit2;
 
     for (i = 0; i <= 99; i++)
     {
-        firstDigit1 = i / 10;
-        secondDigit1 = i % 10;
-
         for (j = i; j <= 99; j++)
         {
-            firstDigit2 = j / 10;
-            secondDigit2 = j % 10;
-
-            putchar(firstDigit1 / 10 + '0');
-            putchar(firstDigit1 % 10 + '0');
-            putchar(secondDigit1 / 10 + '0');
-            putchar(secondDigit1 % 10 + '0');
+            putchar((i / 10) / 10 + '0');
+            putchar((i / 10) % 10 + '0');
+            putchar((i % 10) / 10 + '0');
+            putchar((i % 10) % 10 + '0');
             putchar(' ');
 
-            putchar(firstDigit2 / 10 + '0');
-            putchar(firstDigit2 % 10 + '0');
-            putchar(secondDigit2 / 10 + '0');
-            putchar(secondDigit2 % 10 + '0');
+            putchar((j / 10) / 10 + '0');
+            putchar((j / 10) % 10 + '0');
+            putchar((j % 10) / 10 + '0');
+            putchar((j % 10) % 10 + '0');
+
             if (i != 99 || j != 99)
             {
                 putchar(',');
                 putchar(' ');
             }
-        }
-    }
 
-    putchar('\n');
+            break;
+        }
+
+        if (i != 99)
+        {
+            putchar('\n');
+        }
+
+        break;
+    }
 
     return 0;
 }
