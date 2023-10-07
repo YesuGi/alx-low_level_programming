a14fdd79fe9b7d6a74e6dd2260b7c6c1b1abb184
YesuGi <yesunehgizaw@gmail.com>
2023-10-06 18:21:45 -0700

    if else project alx

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,26 +1,33 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible combinations of three-digit numbers.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    int i, j, k;
+	int i, j, k;
 
-    for (i = 0; i <= 7; i++)
-    {
-        for (j = i + 1; j <= 8; j++)
-        {
-            for (k = j + 1; k <= 9; k++)
-            {
-                printf("%d%d%d", i, j, k);
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = i + 1; j <= 8; j++)
+		{
+			for (k = j + 1; k <= 9; k++)
+			{
+				printf("%d%d%d", i, j, k);
 
-                if (i != 7 || j != 8 || k != 9)
-                {
-                    printf(", ");
-                }
-            }
-        }
-    }
+				if (i != 7 || j != 8 || k != 9)
+				{
+					printf(", ");
+				}
+			}
+		}
+	}
 
-    printf("\n");
+	printf("\n");
 
-    return (0);
+	return (0);
 }
