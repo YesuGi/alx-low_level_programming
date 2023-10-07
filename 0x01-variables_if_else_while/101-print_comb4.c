ff1356553a6da003e547931b0498f709b499fd49
YesuGi <yesunehgizaw@gmail.com>
2023-10-06 18:11:41 -0700

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,20 +1,34 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase.
+ * main - Prints all possible combinations of four-digit numbers.
  *
  * Return: Always 0 (Success).
  */
 int main(void)
 {
-    char letter;
+    int i, j, k, l;
 
-    for (letter = 'a'; letter <= 'z'; letter++)
+    for (i = 0; i <= 9; i++)
     {
-        putchar(letter);
+        for (j = i + 1; j <= 9; j++)
+        {
+            for (k = j + 1; k <= 9; k++)
+            {
+                for (l = k + 1; l <= 9; l++)
+                {
+                    printf("%d%d%d%d", i, j, k, l);
+
+                    if (i != 6 || j != 7 || k != 8 || l != 9)
+                    {
+                        printf(", ");
+                    }
+                }
+            }
+        }
     }
 
-    putchar('\n');
+    printf("\n");
 
     return (0);
 }
